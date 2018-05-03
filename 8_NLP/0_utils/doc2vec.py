a048cd7e70d359498706cdc34eeefcaf1a27edfd
尐 <313303303@qq.com>
2018-05-03 09:32:47 +0800

    Update doc2vec.py

--- 8_NLP/0_utils/doc2vec.py
+++ 8_NLP/0_utils/doc2vec.py
@@ -62,3 +62,29 @@ class MyDoc2Vec(Doc2Vec):
             model.save(path)
         else:
             model.save('./%s___%s.model' % (str(datetime.datetime.today())[:22], model.__str__()))
+
+# class MyDoc2Vec(Doc2Vec):
+#     def __init__(self, corpus, **kwargs):
+#
+#         self.corpus(corpus)
+#         super().__init__(documents=self.corpus, **kwargs)  # documents=self.documents
+#
+#     def corpus(self, corpus):
+#         if isinstance(self.corpus, str) and Path(corpus).is_file():
+#             self.corpus = tqdm(TaggedLineDocument(corpus), desc='TaggedLineDocument')
+#         else:
+#             self.corpus = tqdm(self.CorpusIter(corpus), desc='TaggedDocument')
+#
+#     class CorpusIter(object):
+#         def __init__(self, corpus):
+#             self.corpus = corpus
+#
+#         def __iter__(self):
+#             for idx, line in enumerate(self.corpus):
+#                 yield TaggedDocument(line.split(), [idx])
+#
+#                 # @property
+#                 # def _corpus(self):
+#                 #     """
+#                 #     ['Well done!', 'Good work']
+#                 #     """""
