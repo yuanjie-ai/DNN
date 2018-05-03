07dd6ad770bd5a11bbf03c377516caf785967f4a
尐 <313303303@qq.com>
2018-05-03 09:23:01 +0800

    Update doc2vec.py

--- 8_NLP/0_utils/doc2vec.py
+++ 8_NLP/0_utils/doc2vec.py
@@ -8,16 +8,16 @@ from tqdm import tqdm
 class MyDoc2Vec(Doc2Vec):
     def __init__(self, corpus, **kwargs):
         """
-        docs = ['Well done!',
-                'Good work',
-                'Great effort',
-                'nice work',
-                'Excellent!',
-                'Weak',
-                'Poor effort!',
-                'not good',
-                'poor work',
-                'Could have done better.']
+        docs = [['Well', 'done!'],
+                ['Good', 'work'],
+                ['Great', 'effort'],
+                ['nice', 'work'],
+                ['Excellent!'],
+                ['Weak'],
+                ['Poor', 'effort!'],
+                ['not', 'good'],
+                ['poor', 'work'],
+                ['Could', 'have', 'done', 'better.']]
         model = MyDoc2Vec(docs)
         model.doc2vec()
         """
@@ -55,44 +55,10 @@ class MyDoc2Vec(Doc2Vec):
         if isinstance(self.corpus, str) and Path(self.corpus).is_file():
             self.corpus = TaggedLineDocument(self.corpus)
         else:
-            self.corpus = [TaggedDocument(line.split(), [idx]) for idx, line in enumerate(self.corpus)]
+            self.corpus = [TaggedDocument(line, [idx]) for idx, line in enumerate(self.corpus)]
 
     def model_save(self, model, path=None):
         if path:
             model.save(path)
         else:
             model.save('./%s___%s.model' % (str(datetime.datetime.today())[:22], model.__str__()))
-
-# from pathlib import Path
-#
-# from gensim.models.doc2vec import Doc2Vec, TaggedDocument, TaggedLineDocument
-# from tqdm import *
-#
-#
-# class MyDoc2Vec(Doc2Vec):
-#     def __init__(self, corpus, **kwargs):
-#
-#         self.corpus(corpus)
-#         super().__init__(documents=self.corpus, **kwargs)  # documents=self.documents
-#
-#     def corpus(self, corpus):
-#         if isinstance(self.corpus, str) and Path(corpus).is_file():
-#             self.corpus = tqdm(TaggedLineDocument(corpus), desc='TaggedLineDocument')
-#         else:
-#             self.corpus = tqdm(self.CorpusIter(corpus), desc='TaggedDocument')
-#
-#     class CorpusIter(object):
-#         def __init__(self, corpus):
-#             self.corpus = corpus
-#
-#         def __iter__(self):
-#             for idx, line in enumerate(self.corpus):
-#                 yield TaggedDocument(line.split(), [idx])
-#
-#                 # @property
-#                 # def _corpus(self):
-#                 #     """
-#                 #     ['Well done!', 'Good work']
-#                 #     """""
-#                 #     for idx, line in enumerate(self.corpus):
-#                 #         yield TaggedDocument(line.split(), [idx])
