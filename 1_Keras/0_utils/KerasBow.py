99af55af5578fae8b17abbc321ce8f2b0b57603b
尐 <313303303@qq.com>
2018-06-14 17:28:34 +0800

    Update KerasBow.py

--- 1_Keras/0_utils/KerasBow.py
+++ 1_Keras/0_utils/KerasBow.py
@@ -5,21 +5,21 @@ from tqdm import tqdm
 
 
 class KerasBow(object):
-    def __init__(self, X=None, y=None, maxlen=None, num_words=10 ** 5):
+    def __init__(self, train=None, test=None, maxlen=None, num_words=10 ** 5):
         self.maxlen = maxlen
         self.num_words = num_words
-        self.__preprocessing(texts=tqdm(X, desc="Text Preprocessing"))
-        if y is not None:
-            self.y = to_categorical(y)
+        self.__preprocessing(tqdm(train, desc="Text Preprocessing"), test)
 
-    def __preprocessing(self, texts):
+    def __preprocessing(self, train, test=None):
         """
-        :param texts: ['some thing to do', 'some thing to drink'] 与 sklearn 一致
+        :param train: ['some thing to do', 'some thing to drink'] 与 sklearn 一致
         :return:
         """
         tokenizer = Tokenizer(self.num_words)  # 保留top num_words-1(词频降序)：最常见的num_words-1词
-        tokenizer.fit_on_texts(texts)
-        sequences = tokenizer.texts_to_sequences(texts)
+        tokenizer.fit_on_texts(train)
+        if test is None:
+            test = train
+        sequences = tokenizer.texts_to_sequences(train)
         self.tokenizer = tokenizer
         self.word_index = tokenizer.word_index
         # self.word_counts = tokenizer.word_counts
