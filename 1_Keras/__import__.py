f28c511ec3a23ba5675c82a5a4ad130d6a49b2af
尐 <313303303@qq.com>
2018-07-02 10:27:55 +0800

    Update __import__.py

--- 1_Keras/__import__.py
+++ 1_Keras/__import__.py
@@ -1,4 +1,4 @@
 import keras.backend as K
-from keras.layers import Embedding, Flatten, GRU, Input, LSTM, Lambda
+from keras.layers import Dropout, Dense, Embedding, Flatten, GRU, Input, LSTM, Lambda
 from keras.models import Model
 from keras.optimizers import Adam
